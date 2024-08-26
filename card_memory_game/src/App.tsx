3d3146d16dfc63b1287ff25e186971d84486e8d6
sayanok <rng.jhn.osca@gmail.com>
2024-08-26 14:36:08 +0900

    add おおまかな神経衰弱の動き

--- card_memory_game/src/App.tsx
+++ card_memory_game/src/App.tsx
@@ -1,40 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const App: React.FC = () => {
-  const cardList = ["a", "b", "a", "b"];
-  const [selectedCard, setSelectedCard] = useState<string | null>();
+  const cardList = ["1a", "2b", "3a", "4b", "1a", "2b", "3a", "4b"];
+  const [shuffledCardList, setShuffledCardList] = useState<Array<string>>([]);
+  const [firstSelectedCard, setFirstSelectedCard] = useState<string | null>();
+  const [secondSelectedCard, setSecondSelectedCard] = useState<string | null>();
   const [cardsYouGot, setCardsYouGot] = useState<Array<string>>([]);
   const [timer, setTimer] = useState<number>();
   const [playingTheGame, setPlayingTheGame] = useState<boolean>(false);
-
-  // useEffect(() => {
-  //   shuffleCards();
-  // 開始ボタン押したらシャッフルでも良い？
-  // });
-
-  function shuffleAndSetCards() {
-    // カードをシャッフルしてセットする
-  }
-
-  function onClickHandler(card: string) {
-    if (selectedCard) {
-      setSelectedCard(card);
-    } else {
-      compareSelectedCard(card);
-      setSelectedCard(null);
-    }
-
-    cardList.length === cardsYouGot.length || exitTheGame();
-  }
-
-  function startTimer() {
-    // タイマー開始
-    // タイムアウトした場合の処理はここにかく...?
-  }
-
-  function stopTimer() {
-    // タイマーを停止する
-  }
 
   function startTheGame() {
     setPlayingTheGame(true);
@@ -50,11 +23,52 @@ const App: React.FC = () => {
     stopTimer();
   }
 
-  function compareSelectedCard(card: string) {
-    if (selectedCard === card) {
-      // cardとselectedCardをcardsYouGotにセットする
+  function shuffleAndSetCards() {
+    let tempCardArray: Array<string> = [...cardList];
+
+    for (let n = 0; n < tempCardArray.length; n++) {
+      let tempNum = Math.floor(Math.random() * (cardList.length - 1));
+      let tempStorage = tempCardArray[n];
+
+      tempCardArray[n] = tempCardArray[tempNum];
+      tempCardArray[tempNum] = tempStorage;
+      setShuffledCardList([...tempCardArray]);
+    }
+  }
+
+  function onClickHandler(card: string) {
+    if (firstSelectedCard && secondSelectedCard) {
+      setFirstSelectedCard(null);
+      setSecondSelectedCard(null);
+    }
+
+    // 思ったように動いていない
+    if (!firstSelectedCard) {
+      const tempCard = card;
+      setFirstSelectedCard(tempCard);
     } else {
-      // 何もしなくて良いのか...？
+      compareFirstSelectedCard(card);
+    }
+
+    cardList.length === cardsYouGot.length || exitTheGame();
+  }
+
+  function startTimer() {
+    // タイマー開始fv /:v;.bv.ds/:;;d:][dx:][　]
+    // タイムアウトした場合の処理はここにかく...?
+  }
+
+  function stopTimer() {
+    // タイマーを停止する
+  }
+
+  function compareFirstSelectedCard(card: string) {
+    if (firstSelectedCard === card) {
+      setCardsYouGot([...cardsYouGot, firstSelectedCard, card]);
+      console.log(cardsYouGot);
+    } else {
+      const tempCard = card;
+      setSecondSelectedCard(tempCard);
     }
   }
 
@@ -62,15 +76,16 @@ const App: React.FC = () => {
     <>
       <button onClick={() => startTheGame()}>start</button>
       <div>{timer}</div>
-      {cardList.map((card) => {
-        <div
-          onClick={() => {
-            onClickHandler(card);
-          }}
-        >
+      <div>{playingTheGame ? "start" : "not start"}</div>
+      {shuffledCardList.map((card) => (
+        <button onClick={() => onClickHandler(card)} disabled={cardsYouGot.includes(card)}>
           {card}
-        </div>;
-      })}
+        </button>
+      ))}
+      <div>1枚目{firstSelectedCard}</div>
+      <div>2枚目{secondSelectedCard}</div>
+
+      <div>獲得したカード{cardsYouGot}</div>
     </>
   );
 };
