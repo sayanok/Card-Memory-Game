550dd66590e702015822ec353ca25ae9df37ca45
sayanok <rng.jhn.osca@gmail.com>
2024-09-01 16:46:40 +0900

    modify デザインなど

--- card_memory_game/src/App.tsx
+++ card_memory_game/src/App.tsx
@@ -1,29 +1,34 @@
+import { DialogActions, DialogContent } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
 import React, { useState } from "react";
+import AbcIcon from "@mui/icons-material/Abc";
+import { Anchor, AutoAwesome, Castle } from "@mui/icons-material";
+
+interface Card {
+  id: number;
+  icon: JSX.Element;
+  cardName: string;
+  isClickable: boolean;
+  isHidden: boolean;
+  isObtained: boolean;
+}
 
 const App: React.FC = () => {
   const cardList = [
-    { id: 0, card: "1a", isClickable: true, isHidden: true, isObtained: false },
-    { id: 1, card: "2b", isClickable: true, isHidden: true, isObtained: false },
-    { id: 2, card: "1a", isClickable: true, isHidden: true, isObtained: false },
-    { id: 3, card: "2b", isClickable: true, isHidden: true, isObtained: false },
+    { id: 0, icon: <AbcIcon />, cardName: "Abc", isClickable: true, isHidden: true, isObtained: false },
+    { id: 1, icon: <AbcIcon />, cardName: "Abc", isClickable: true, isHidden: true, isObtained: false },
+    { id: 2, icon: <Anchor />, cardName: "Anchor", isClickable: true, isHidden: true, isObtained: false },
+    { id: 3, icon: <Anchor />, cardName: "Anchor", isClickable: true, isHidden: true, isObtained: false },
+    { id: 4, icon: <AutoAwesome />, cardName: "AutoAwesome", isClickable: true, isHidden: true, isObtained: false },
+    { id: 5, icon: <AutoAwesome />, cardName: "AutoAwesome", isClickable: true, isHidden: true, isObtained: false },
+    { id: 6, icon: <Castle />, cardName: "Castle", isClickable: true, isHidden: true, isObtained: false },
+    { id: 7, icon: <Castle />, cardName: "Castle", isClickable: true, isHidden: true, isObtained: false },
   ];
-  const [shuffledCardList, setShuffledCardList] = useState<
-    Array<{ id: number; card: string; isClickable: boolean; isHidden: boolean; isObtained: boolean }>
-  >([]);
-  const [firstSelectedCard, setFirstSelectedCard] = useState<{
-    id: number;
-    card: string;
-    isClickable: boolean;
-    isHidden: boolean;
-    isObtained: boolean;
-  } | null>();
-  const [secondSelectedCard, setSecondSelectedCard] = useState<{
-    id: number;
-    card: string;
-    isClickable: boolean;
-    isHidden: boolean;
-    isObtained: boolean;
-  } | null>();
+  const [shuffledCardList, setShuffledCardList] = useState<Array<Card>>([]);
+  const [firstSelectedCard, setFirstSelectedCard] = useState<Card | null>();
+  const [secondSelectedCard, setSecondSelectedCard] = useState<Card | null>();
   const [cardsYouGot, setCardsYouGot] = useState<Array<string>>([]);
 
   const [playingTheGame, setPlayingTheGame] = useState<boolean>(false);
@@ -40,8 +45,8 @@ const App: React.FC = () => {
   ////////////////////// ゲームステータスに関する機能　//////////////////////
   function startTheGame() {
     setPlayingTheGame(true);
-    startTimer();
     shuffleAndSetCards();
+    startTimer();
     setResult("");
   }
 
@@ -53,13 +58,7 @@ const App: React.FC = () => {
 
   ////////////////////// 神経衰弱に関する機能 //////////////////////
   function shuffleAndSetCards() {
-    let tempCardArray: Array<{
-      id: number;
-      card: string;
-      isClickable: boolean;
-      isHidden: boolean;
-      isObtained: boolean;
-    }> = [...cardList];
+    let tempCardArray: Array<Card> = [...cardList];
 
     for (let n = 0; n < tempCardArray.length; n++) {
       let tempNum = Math.floor(Math.random() * (cardList.length - 1));
@@ -71,14 +70,13 @@ const App: React.FC = () => {
     }
   }
 
-  function onClickHandler(card: {
-    id: number;
-    card: string;
-    isClickable: boolean;
-    isHidden: boolean;
-    isObtained: boolean;
-  }) {
-    shuffledCardList.map((element) => element.id === card.id && (element.isClickable = false));
+  function onClickHandler(card: Card) {
+    shuffledCardList.map((element) => {
+      if (element.id === card.id) {
+        element.isClickable = false;
+        element.isHidden = false;
+      }
+    });
 
     if (!firstSelectedCard) {
       setFirstSelectedCard(card);
@@ -88,14 +86,10 @@ const App: React.FC = () => {
     }
   }
 
-  function compareSelectedCard(card: {
-    id: number;
-    card: string;
-    isClickable: boolean;
-    isHidden: boolean;
-    isObtained: boolean;
-  }) {
-    if (firstSelectedCard && firstSelectedCard.card === card.card) {
+  function compareSelectedCard(card: Card) {
+    console.log(firstSelectedCard && firstSelectedCard.cardName);
+    console.log(card.cardName);
+    if (firstSelectedCard && firstSelectedCard.cardName === card.cardName) {
       shuffledCardList.map((element) => {
         if (element.id === firstSelectedCard.id) {
           element.isHidden = false;
@@ -111,8 +105,8 @@ const App: React.FC = () => {
       });
 
       let tempCardsYouGot = cardsYouGot;
-      tempCardsYouGot.push(firstSelectedCard.card);
-      tempCardsYouGot.push(card.card);
+      tempCardsYouGot.push(firstSelectedCard.cardName);
+      tempCardsYouGot.push(card.cardName);
       setCardsYouGot([...tempCardsYouGot]);
       cardsYouGot.length === cardList.length && exitTheGame("ゲームクリア");
     } else {
@@ -120,22 +114,34 @@ const App: React.FC = () => {
         shuffledCardList.map((element) => {
           if (firstSelectedCard && element.id === firstSelectedCard.id) {
             element.isClickable = true;
+            element.isHidden = true;
           }
         });
 
         shuffledCardList.map((element) => {
           if (element.id === card.id) {
             element.isClickable = true;
+            element.isHidden = true;
           }
         });
-      }, 1000);
-      // 1000の間に他のカードをクリックすると挙動がおかしくなる
+
+        shuffledCardList.map((card) => {
+          if (!card.isObtained) {
+            card.isClickable = false;
+          }
+        });
+      }, 500);
     }
 
     setTimeout(() => {
       setFirstSelectedCard(null);
       setSecondSelectedCard(null);
-    }, 1000);
+      shuffledCardList.map((element) => {
+        if (!element.isObtained) {
+          element.isClickable = true;
+        }
+      });
+    }, 500);
   }
 
   ////////////////////// タイマーに関する機能 //////////////////////
@@ -151,6 +157,12 @@ const App: React.FC = () => {
 
     timeoutId = setTimeout(() => {
       exitTheGame("ゲームオーバー");
+      setShuffledCardList((shuffledCardList) =>
+        shuffledCardList.map((card) => {
+          card.isClickable = false;
+          return card;
+        })
+      );
     }, timeLimit * 1000);
     setTimeoutIdForGameClear(timeoutId);
   }
@@ -173,6 +185,10 @@ const App: React.FC = () => {
     setTimeoutIdForGameClear(timeoutId);
   }
 
+  const handleClose = () => {
+    setResult("");
+  };
+
   return (
     <>
       <button onClick={() => startTheGame()} disabled={playingTheGame}>
@@ -180,17 +196,27 @@ const App: React.FC = () => {
       </button>
 
       <div>{time}</div>
-      {shuffledCardList.map((card) => (
-        <button onClick={() => onClickHandler(card)} disabled={!card.isClickable}>
-          {card.card}
-        </button>
-      ))}
-      <div>1枚目{firstSelectedCard && firstSelectedCard.card}</div>
-      <div>2枚目{secondSelectedCard && secondSelectedCard.card}</div>
+      <Grid container spacing={2}>
+        {shuffledCardList.map((card) => (
+          <Grid size={4}>
+            <Button variant="contained" size="large" onClick={() => onClickHandler(card)} disabled={!card.isClickable}>
+              {card.isHidden ? "　　" : <>{card.icon}</>}
+            </Button>
+          </Grid>
+        ))}
+      </Grid>
 
-      <div>獲得したカード{cardsYouGot}</div>
-      <div>結果{result}</div>
-      <div hidden={result !== "ゲームクリア"}>ゲームクリアにかかった時間:{timeLimit - time}</div>
+      <div>{result}</div>
+
+      <Dialog open={result === "ゲームクリア"} onClose={handleClose}>
+        <DialogContent>
+          {result}
+          <div>おめでとうございます。{timeLimit - time}秒でクリアしました！</div>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>閉じる</Button>
+        </DialogActions>
+      </Dialog>
     </>
   );
 };
